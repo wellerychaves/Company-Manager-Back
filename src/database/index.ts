2bb2f993ff4a262071aecd1345c28526dafa06a0
wellerychaves <wellerychaves@outlook.com>
2025-01-19 13:52:41 -0300

    chore: remove comments from the code

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,13 +1,5 @@
 import { drizzle } from "drizzle-orm/postgres-js";
-import postgres from 'postgres';
-// import { users } from "./schemas/users.schema";
+import postgres from "postgres";
 
 const queryClient = postgres(process.env.DATABASE_URL);
 export const db = drizzle({ client: queryClient });
-
-/* 
-const result = await db.execute('select 1');
-
-const usersCount = await db.$count(users);
-console.log(usersCount);
-*/
