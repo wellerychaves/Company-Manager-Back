36ed48725b7d55ba3d267197933545aec071a850
wellerychaves <wellerychaves@outlook.com>
2025-01-19 12:19:55 -0300

    feat: will add a new relation with products table

--- src/database/schemas/companies.schema.ts
+++ src/database/schemas/companies.schema.ts
@@ -1,32 +1,25 @@
 import { relations } from "drizzle-orm";
 import { boolean, pgTable, text, varchar } from "drizzle-orm/pg-core";
 import { timestampsDrizzle } from "../helpers/timestamps.helpers";
+import { productsTable } from "./products.schema";
 import { usersTable } from "./users.schema";
 
 export const companiesTable = pgTable("companies", {
 	id: varchar().primaryKey(),
 	userId: varchar().notNull(),
 	companyName: varchar().notNull(),
-	companyEmail: varchar().notNull(),
-	contactPhone: varchar().notNull(),
+	companyEmail: varchar().notNull().unique(),
+	contactPhone: varchar().notNull().unique(),
 	address: varchar().notNull(),
 	description: text(),
 	status: boolean().default(false),
 	...timestampsDrizzle,
 });
 
-export const companiesRelations = relations(companiesTable, ({ one }) => ({
+export const companiesRelations = relations(companiesTable, ({ one, many }) => ({
 	user: one(usersTable, {
 		fields: [companiesTable.userId],
 		references: [usersTable.id],
 	}),
+	products: many(productsTable),
 }));
-
-/* 
-	https://orm.drizzle.team/docs/relations
-	id: varchar().primaryKey(),
-	companyName: st
-	userId: string que irá conter o uuid a quem a empresa pertence
-	productsIDs: array de strings que conterão os produtos 
-
-*/
