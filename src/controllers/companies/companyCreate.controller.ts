d027a0ff444dc068a6f5607333fbc6060f15245c
wellerychaves <wellerychaves@outlook.com>
2025-01-19 11:55:11 -0300

    refactor: change the props name to make more sense

--- src/controllers/companies/companyCreate.controller.ts
+++ src/controllers/companies/companyCreate.controller.ts
@@ -1,9 +1,12 @@
 import type { Context } from "hono";
+import type { ICreateCompanySchema } from "../../interfaces/company.interface";
 import { createCompanyService } from "../../services/companies/companyCreate.service";
 
 export const createCompanyController = async (c: Context) => {
+	const data: ICreateCompanySchema = await c.req.json();
+
 	try {
-		const company = await createCompanyService(await c.req.json());
+		const company = await createCompanyService(data);
 
 		return c.json(company, 201);
 	} catch (err) {
