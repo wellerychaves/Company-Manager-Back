5137b5bdad7a1e447761743915f9499baecbe122
wellerychaves <wellerychaves@outlook.com>
2025-01-19 12:26:54 -0300

    feat: improve schema
    the schema now have a new object and was added a check to the quantity
    to always be positive. A new Interface was added also

--- src/interfaces/produtct.interface.ts
+++ src/interfaces/produtct.interface.ts
@@ -1,14 +1,23 @@
 import { z } from "zod";
+import { timestampsZod } from "../database/helpers/timestamps.helpers";
 
 export const productSchema = z.object({
 	id: z.string().uuid(),
 	companyId: z.string(),
 	productName: z.string(),
-	quantity: z.number().int(),
-	price: z.number().positive(),
-	location: z.string().optional(),
-	createdAt: z.date().default(new Date()),
-	updatedAt: z.date().default(new Date()),
+	quantity: z.number().int().positive(),
+	price: z.bigint().positive(),
+	description: z.string(),
+	...timestampsZod,
 });
 
-export type Iproduct = z.infer<typeof productSchema>;
+export const createProductSchema = productSchema.pick({
+	companyId: true,
+	productName: true,
+	quantity: true,
+	price: true,
+	description: true,
+});
+
+export type IProduct = z.infer<typeof productSchema>;
+export type ICreateProductSchema = z.infer<typeof createProductSchema>;
