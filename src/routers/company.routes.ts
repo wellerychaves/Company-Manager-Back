8710c40b473eab2e3833a9d009d48ee4ca7cdcc0
wellerychaves <wellerychaves@outlook.com>
2025-01-19 13:49:28 -0300

    feat: add new routes

--- src/routers/company.routes.ts
+++ src/routers/company.routes.ts
@@ -1,14 +1,22 @@
 import { zValidator } from "@hono/zod-validator";
 import { Hono } from "hono";
+
+import { createCompanySchema } from "../interfaces/company.interface";
+
 import { createCompanyController } from "../controllers/companies/companyCreate.controller";
 import { deleteCompanyController } from "../controllers/companies/companyDelete.controller";
 import { listCompaniesController } from "../controllers/companies/companyList.controller";
 import { listOneCompanyController } from "../controllers/companies/companyListOne.controller";
-import { createCompanySchema } from "../interfaces/company.interface";
+import { updateCompanyController } from "../controllers/companies/companyUpdate.controller";
 
 export const company = new Hono();
 
 company.get("/", (c) => listCompaniesController(c));
 company.get("/:id", (c) => listOneCompanyController(c));
 company.post("/", zValidator("json", createCompanySchema), (c) => createCompanyController(c));
+company.patch("/:id", (c) => updateCompanyController(c));
 company.delete("/:id", (c) => deleteCompanyController(c));
+
+company.notFound((c) => {
+	return c.text("Not Found", 404);
+});
--- src/routers/product.routes.ts
+++ src/routers/product.routes.ts
@@ -1,8 +1,22 @@
+import { zValidator } from "@hono/zod-validator";
 import { Hono } from "hono";
+
+import { createProductSchema } from "../interfaces/produtct.interface";
+
+import { createProductController } from "../controllers/products/productCreate.controller";
+import { deleteProductController } from "../controllers/products/productDelete.controller";
+import { listProductController } from "../controllers/products/productList.controller";
+import { listOneProductController } from "../controllers/products/productListOne.controller";
+import { updateProductController } from "../controllers/products/productUpdate.controller";
 
 export const product = new Hono();
 
-product.get("/", (c) => c.json({ message: "List of products" }));
+product.get("/", (c) => listProductController(c));
+product.get("/:id", (c) => listOneProductController(c));
+product.post("/", zValidator("json", createProductSchema), (c) => createProductController(c));
+product.patch("/:id", (c) => updateProductController(c));
+product.delete("/:id", (c) => deleteProductController(c));
+
 product.notFound((c) => {
-	return c.text("Product doesn't exists", 404);
+	return c.text("Not Found", 404);
 });
