6cb033e3df6b1a3c7b8fef3cfe151f15010b9a76
wellerychaves <wellerychaves@outlook.com>
2025-01-19 13:50:36 -0300

    fix: will make the error message more reasonable

--- src/services/companies/companyListOne.service.ts
+++ src/services/companies/companyListOne.service.ts
@@ -3,11 +3,14 @@ import { db } from "../../database";
 import { companiesTable } from "../../database/schemas/companies.schema";
 
 export const listOneCompanyService = async (companyId: string) => {
-	const user = await db.select().from(companiesTable).where(eq(companiesTable.id, companyId));
+	try {
+		const company = await db.select().from(companiesTable).where(eq(companiesTable.id, companyId));
 
-	if (user.length === 0) {
-		throw new Error("Company does not exist in database");
+		if (company.length === 0) {
+			throw new Error("This company does not exist in database");
+		}
+		return company;
+	} catch (err) {
+		throw new Error(`${err.message}`);
 	}
-
-	return user;
 };
