e0fdf6293aadad8fff81f012fd7e7cbdafbae0e7
wellerychaves <wellerychaves@outlook.com>
2025-02-17 18:18:05 -0300

    feat(sales): add more typing fields

--- src/interfaces/sales.interface.ts
+++ src/interfaces/sales.interface.ts
@@ -2,10 +2,12 @@ import { z } from "zod";
 
 export const salesSchema = z.object({
 	id: z.string().uuid(),
+	companyId: z.string(),
 	productId: z.string(),
 	salesDate: z.date(),
-	salePrice: z.number().positive(),
-	systemCommission: z.number().positive(),
+	unitPrice: z.number().positive(),
+	totalPrice: z.number().positive(),
+	paymentMethod: z.string(),
 	createdAt: z.date().default(new Date()),
 	updatedAt: z.date().default(new Date()),
 });
