e6b3c81b31049c58adb0127766bcfca37b6306ba
wellerychaves <wellerychaves@outlook.com>
2025-02-17 19:15:23 -0300

    refactor: remove console.log

--- src/utils/validations/login.validation.ts
+++ src/utils/validations/login.validation.ts
@@ -1,8 +1,8 @@
+import type { Context } from "hono";
 import { userLoginSchema } from "../../interfaces/user.interface";
 
-export const loginValidation = async (c) => {
+export const loginValidation = async (c: Context) => {
 	const credentials = await c.req.json();
-	//console.log(credentials, c);
 
 	const parsed = userLoginSchema.safeParse(credentials);
 	if (!parsed.success) {
